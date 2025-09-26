707351a40f49602827119e56f71f6af2ba749611
Odediran Philip Oluseyi <philipoluseyi@gmail.com>
2025-09-26 17:53:28 +0100

    SETUP: technology page set-up

---
+++ app/technology/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+
+const Page = () => {
+    return (
+        <div className="flex items-center justify-center h-screen text-white">
+            Technology Page
+        </div>
+    );
+};
+
+export default Page;

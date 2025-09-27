87c3aea8bb3049ce084c6dba15af46cb0d3daeae
Odediran Philip Oluseyi <philipoluseyi@gmail.com>
2025-09-27 05:20:31 +0100

    COMMIT: Added components from roadmap branch

--- app/technology/page.js
+++ app/technology/page.js
@@ -4,14 +4,18 @@ import HeroTechnology from "@/components/technology/hero-technology";
 import DataLayer from "@/components/technology/data-layer";
 import Infrastructure from "@/components/technology/infrastructure";
 import Marketplace from "@/components/technology/marketplace";
+import EverythingRunsThrough from "@/components/roadmap/EverythingRunsThrough";
+import {WhyItMattersSection} from "@/components/roadmap/WhyItMattersSection";
 
 const Page = () => {
     return (
-        <main className="flex items-center flex-col justify-center  text-white">
+        <main  className=" overflow-hidden w-full flex flex-col relative min-h-screen bg-black text-white">
             <HeroTechnology />
             <Marketplace/>
+            <EverythingRunsThrough/>
             <DataLayer/>
             <Infrastructure/>
+            <WhyItMattersSection/>
         </main>
     );
 };
